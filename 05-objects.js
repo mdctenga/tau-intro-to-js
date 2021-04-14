b029456917b3f8299c8740134a406c95c5ae0be8
mdctenga <mdctenga@gmail.com>
2021-04-13 22:17:16 -1000

    create an object literal and a class that uses a constructor

--- 05-objects.js
+++ 05-objects.js
@@ -1,9 +1,27 @@
 const cookie = {
   name: "Chocolate Chip",
   isGlutenFree: false,
-  "+Yummy": true
+  "+Yummy": true,
+  eatCookie: function(){
+    console.log("I am eating " + this.name);
+  }
 };
 
 cookie.name = "Chip Chocolate"; // overwrites the cookie object's name property
 cookie.isGlutenFree = true; //overwrites the cookie object's isGlutenFree property
-console.log(cookie["+Yummy"]);
+// console.log(cookie);
+// console.log(cookie["+Yummy"]);
+cookie.eatCookie(); // accesses the eatCookie property in the object cookie and invokes it. This only works because I know that eatCookie is a function
+
+class Cookie {
+  constructor(name,isGlutenFree){
+    this.name = name;
+    this.isGlutenFree = isGlutenFree;
+  }
+
+  eatCookie(){
+    console.log("I am eating " + this.name);
+  }
+}
+const myCookie = new Cookie("Iced Oatmeal", false);
+myCookie.eatCookie();
